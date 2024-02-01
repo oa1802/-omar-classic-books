fe0ae7dd10e6c6f57f04351cfdf593030e7817d3
Omar Alkhalili <omaralkha@gmail.com>
2024-01-31 22:14:23 -0500

    Update contact.blade.php
    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,7 +9,7 @@
         <br>
         <h2>Contact</h2>
         <br>
-        <p class="contact-text text-center">1 Washington Street, Hoboken, NJ 07030</p>
+        <p class="contact-text text-center">408 Washington Street, Hoboken, NJ 07030</p>
         <p class="contact-text text-center"><strong><a href = "mailto:info@omarclassicbooks.com">info@omarclassicbooks.com</a></strong></p>
         <p class="contact-text text-center">(000) 000-0000</p>
 		</form>
